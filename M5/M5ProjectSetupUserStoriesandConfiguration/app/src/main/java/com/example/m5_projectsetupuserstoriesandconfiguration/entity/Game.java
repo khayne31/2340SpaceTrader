cb04224aba096cc257b0e69aaf1ed792302140c8
unknown <maxjforsyth@gmail.com>
2019-02-17 01:47:27 -0500

    edited enum and main screen for game

--- M5/M5ProjectSetupUserStoriesandConfiguration/app/src/main/java/com/example/m5_projectsetupuserstoriesandconfiguration/entity/Game.java
+++ M5/M5ProjectSetupUserStoriesandConfiguration/app/src/main/java/com/example/m5_projectsetupuserstoriesandconfiguration/entity/Game.java
@@ -1,5 +1,50 @@
 package com.example.m5_projectsetupuserstoriesandconfiguration.entity;
 
 public class Game {
-    private enum  difficulties = {"beginner", "easy", "normal", "hard", "impossble"};
+    private enum  difficulty {
+        BE ("beginner", "BE"),
+        EA ("easy", "EA"),
+        NO ("normal", "NO"),
+        HA ("hard", "HA"),
+        IM ("impossible", "IM");
+
+    private final String level;
+    private final String id;
+
+    difficulty (String difLevel, String difId) {
+        level = difLevel;
+        id = difId;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+    public String getId() {
+        return id;
+    }
+    public String toString() {
+        return id;
+    }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
