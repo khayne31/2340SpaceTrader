c123682c8bad99833f29bd1838075c9188b88022
unknown <maxjforsyth@gmail.com>
2019-02-19 16:36:48 -0500

    adding some classes for backend work

---
+++ M5/M5ProjectSetupUserStoriesandConfiguration/app/src/main/java/com/example/m5_projectsetupuserstoriesandconfiguration/model/Repository.java
@@ -0,0 +1,33 @@
+package com.example.m5_projectsetupuserstoriesandconfiguration.model;
+
+import android.util.Log;
+
+import com.example.m5_projectsetupuserstoriesandconfiguration.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Repository {
+
+    private static int next_id = 1;
+
+    private static int getNextUniqueID() {return next_id++;}
+
+    private List<User> allUsers;
+
+
+    /**
+     * Make a new Repository object
+     */
+    public Repository() {
+        allUsers = new ArrayList<>();
+
+    }
+
+
+    /**
+     * Return all the Users in the system
+     */
+    public List<User> getAllUsers() {return allUsers;}
+}
