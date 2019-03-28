c04fff2b62bd9f4fd28a0aefdc2494089f1d2c1f
Owen McCarty <owenmccarty@lawn-128-61-62-167.lawn.gatech.edu>
2019-03-27 22:23:17 -0400

    max fuel implementation

--- M7/M5ProjectSetupUserStoriesandConfiguration/app/src/main/java/com/example/m5_projectsetupuserstoriesandconfiguration/entity/Ship.java
+++ M7/M5ProjectSetupUserStoriesandConfiguration/app/src/main/java/com/example/m5_projectsetupuserstoriesandconfiguration/entity/Ship.java
@@ -6,16 +6,16 @@ import java.util.List;
 public class Ship {
     public enum shipType {
         //(Name, ID, cargo, range, hull, crew, wp, gad, sh)
-        FL("Flea", "FL", 7, 20, 10, 0, 0, 0, 0), // cargoSize not given (on wiki) supposed to be "few"
-        GN("Gnat", "GN", 15, 14, 20, 0, 1, 1, 0),
-        FI("Firefly", "FI", 20, 17, 25, 0, 1, 1, 1),
-        MO("Mosquito", "MO", 15, 13, 30, 0, 2, 1, 1),
-        BB("Bumblebee", "BB", 20, 15, 25, 1, 1, 2, 2),
-        BE("Beetle", "BE", 50, 14, 10, 3, 0, 1, 1),
-        HO("Hornet", "HO", 20, 16, 30, 2, 3, 1, 2),
-        GR("Grasshopper", "GR", 30, 15, 35, 3, 2, 3, 2),
-        TE("Termite", "TE", 60, 13, 50, 3, 1, 2, 3),
-        WA("Wasp", "WA", 35, 14, 50, 3, 3, 2, 2);
+        FL("Flea", "FL", 7, 20, 10, 0, 0, 0, 0, 500), // cargoSize not given (on wiki) supposed to be "few"
+        GN("Gnat", "GN", 15, 14, 20, 0, 1, 1, 0, 700),
+        FI("Firefly", "FI", 20, 17, 25, 0, 1, 1, 1, 1000),
+        MO("Mosquito", "MO", 15, 13, 30, 0, 2, 1, 1, 2000),
+        BB("Bumblebee", "BB", 20, 15, 25, 1, 1, 2, 2, 3000),
+        BE("Beetle", "BE", 50, 14, 10, 3, 0, 1, 1, 4000),
+        HO("Hornet", "HO", 20, 16, 30, 2, 3, 1, 2, 5000),
+        GR("Grasshopper", "GR", 30, 15, 35, 3, 2, 3, 2, 10000),
+        TE("Termite", "TE", 60, 13, 50, 3, 1, 2, 3, 15000),
+        WA("Wasp", "WA", 35, 14, 50, 3, 3, 2, 2, 20000);
 
         private final String type;
         private final String id;
@@ -26,10 +26,13 @@ public class Ship {
         private final int wpSlots;
         private final int gadSlots;
         private final int shSlots;
+        private final int maxfuel;
+
+
 
         // need hullStrength, crewSize, wpSlots, gadSlots, shSlots
 
-        shipType(String sType, String sId, int cSize, int r, int hull, int crew, int wp, int gad, int sh) {
+        shipType(String sType, String sId, int cSize, int r, int hull, int crew, int wp, int gad, int sh, int max) {
             type = sType;
             id = sId;
             cargoSize = cSize;
@@ -39,6 +42,7 @@ public class Ship {
             wpSlots = wp;
             gadSlots = gad;
             shSlots = sh;
+            maxfuel = max;
         }
 
         public int getRange() {
@@ -104,6 +108,7 @@ public class Ship {
     private Hashtable<GoodType, Integer> goodList;
     private List<Item> itemList;
     private int range;
+    private int maxfuel;
 
 
     public Ship(shipType type, int fuel, int hp, int cargo) {
@@ -111,6 +116,7 @@ public class Ship {
         this.fuel = fuel;
         this.hp = hp;
         this.cargo = cargo;
+        maxfuel = type.maxfuel;
         goodList = new Hashtable<>();
         this.range = this.type.getRange();
         initializeGoods();
@@ -169,8 +175,13 @@ public class Ship {
         return type;
     }
 
+    /*
+    sets the fuel to the amount specified if less than maximum capacity
+     */
     public void setFuel(int fuel) {
-        this.fuel = fuel;
+        if (fuel < this.maxfuel) {
+            this.fuel = fuel;
+        }
     }
 
     public int getFuel() {
