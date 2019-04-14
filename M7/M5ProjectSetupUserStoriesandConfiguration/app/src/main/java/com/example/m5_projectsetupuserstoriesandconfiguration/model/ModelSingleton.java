dde6cda4674d514fedbb6250b6a9eba74f966206
Owen McCarty <owenmccarty@lawn-128-61-60-209.lawn.gatech.edu>
2019-04-14 11:43:41 -0400

    fixed non-compiling gradle stuff

--- M7/M5ProjectSetupUserStoriesandConfiguration/app/src/main/java/com/example/m5_projectsetupuserstoriesandconfiguration/model/ModelSingleton.java
+++ M7/M5ProjectSetupUserStoriesandConfiguration/app/src/main/java/com/example/m5_projectsetupuserstoriesandconfiguration/model/ModelSingleton.java
@@ -6,6 +6,9 @@ import com.example.m5_projectsetupuserstoriesandconfiguration.entity.Market;
 import com.example.m5_projectsetupuserstoriesandconfiguration.entity.Player;
 import com.example.m5_projectsetupuserstoriesandconfiguration.entity.Player2;
 import com.example.m5_projectsetupuserstoriesandconfiguration.entity.SerializableStorage;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 //import com.google.firebase.database.DatabaseReference;
 //import com.google.firebase.database.FirebaseDatabase;
 //import com.google.firebase.database.DatabaseReference;
@@ -179,20 +182,19 @@ public class ModelSingleton implements Serializable {
     public boolean saveBinary(FileOutputStream file) {
 
         Log.d("SINGLETON", "saveBinary");
+        
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
 
-//FirebaseApp.initializeApp(this);
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference();
-//
-//
-//        myRef.child("1").setValue("test2");
-//        Player player = getPlayerInteractor().getAllPlayers().get(0);
-//        Player2 player2 = new Player2(player, player.getName(), player.getFighterPoints(),
-//                player.getTraderPoints(), player.getEngineerPoints(), player.getPilotPoints(),
-//                player.getDiff());
-//
-//        myRef.child("p").child("player").
-//                setValue(player2);
+
+        myRef.child("1").setValue("test2");
+        Player player = getPlayerInteractor().getAllPlayers().get(0);
+        Player2 player2 = new Player2(player, player.getName(), player.getFighterPoints(),
+                player.getTraderPoints(), player.getEngineerPoints(), player.getPilotPoints(),
+                player.getDiff());
+
+        myRef.child("p").child("player").
+                setValue(player2);
 
 //myRef.child("0").child("playerUniverse").setValue(player.getCurrentUniverse());
 
