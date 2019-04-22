81717b3e1cfcbc65029b283e52cae76238952805
unknown <maxjforsyth@gmail.com>
2019-04-21 22:48:39 -0400

    started adding combat and pirate classes

---
+++ M7/M5ProjectSetupUserStoriesandConfiguration/app/src/main/java/com/example/m5_projectsetupuserstoriesandconfiguration/entity/Combat.java
@@ -0,0 +1,17 @@
+package com.example.m5_projectsetupuserstoriesandconfiguration.entity;
+
+public class Combat {
+
+    Combat() {
+
+    }
+
+    public String playerAttack(Ship playerShip) {
+        String returnString;
+        returnString = "";
+
+        return returnString;
+    }
+
+
+}
