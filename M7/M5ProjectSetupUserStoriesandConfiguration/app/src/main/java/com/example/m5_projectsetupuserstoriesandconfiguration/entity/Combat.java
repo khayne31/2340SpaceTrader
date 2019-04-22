2053185675c0248c169862381c61d468d2047351
unknown <maxjforsyth@gmail.com>
2019-04-22 00:56:51 -0400

    more combat updates

--- M7/M5ProjectSetupUserStoriesandConfiguration/app/src/main/java/com/example/m5_projectsetupuserstoriesandconfiguration/entity/Combat.java
+++ M7/M5ProjectSetupUserStoriesandConfiguration/app/src/main/java/com/example/m5_projectsetupuserstoriesandconfiguration/entity/Combat.java
@@ -1,17 +1,60 @@
 package com.example.m5_projectsetupuserstoriesandconfiguration.entity;
 
+import java.util.Random;
+
 public class Combat {
 
-    Combat() {
+    private Random random;
 
+    Combat() {
+        random = new Random();
     }
 
-    public String playerAttack(Ship playerShip) {
+    /**
+     * A method that lets a player attack a pirate
+     * @param playerShip The player's ship
+     * @return A string saying how much damage was done
+     */
+    public String playerAttackPirate(Ship playerShip) {
         String returnString;
         returnString = "";
 
         return returnString;
     }
 
+    /**
+     * A method that lets a player attack a police ship
+     * @param playerShip The player's ship
+     * @return A string saying how much damage was done
+     */
+    public String playerAttackPolice(Ship playerShip) {
+        String returnString;
+        returnString = "";
 
+        return returnString;
+    }
+
+    /**
+     * A method that lets a player defend from a pirate ship attack
+     * @param playerShip
+     * @return
+     */
+    public String playerDefendPirate(Ship playerShip) {
+        String returnString;
+        returnString = "";
+
+        return returnString;
+    }
+
+    /**
+     * A method that lets a player defend from a police ship attack
+     * @param playerShip
+     * @return
+     */
+    public String playerDefendPolice(Ship playerShip) {
+        String returnString;
+        returnString = "";
+
+        return returnString;
+    }
 }
